2ef38cd78e3943578f5d8733033d35b140812980
Arne Hilmann <arne@hilmann.de>
2015-07-31 17:53:09 +0200

    support multiple instances

--- src/AIExport.c
+++ src/AIExport.c
@@ -24,63 +24,69 @@
 #define COOKIE_PREFIX "erlang_ai"
 
 
-int team_id = -1;
 int frame = -1;
-char node_name[100];
-char cookie[100];
+char node_names[32][100];
 
-ei_cnode ec;
-char hq_node[100];
-int uplink = -1;
-const struct SSkirmishAICallback* callback;
+char cookies[32][100];
+
+ei_cnode ecs[32];
+char hq_nodes[32][100];
+int uplinks[32];
+const struct SSkirmishAICallback* callbacks[32];
 
 
-EXPORT(int) init(int new_team_id, const struct SSkirmishAICallback* new_callback) {
-    team_id = new_team_id;
-    callback = new_callback;
+EXPORT(int) init(int team_id, const struct SSkirmishAICallback* new_callback) {
+    callbacks[team_id] = new_callback;
 
-    fprintf(stdout, "sync version: %s\n", callback->Engine_Version_getFull(team_id));
+    fprintf(stdout, "sync version: %s\n", callbacks[team_id]->Engine_Version_getFull(team_id));
 
     fprintf(stdout, "\n\n\t\tStarting up Erlang CNode, Team %i\n\n", team_id);
 
-    sprintf(node_name, "%s%i", NODE_NAME_PREFIX, team_id);
-    sprintf(cookie, "%s%i", COOKIE_PREFIX, team_id);
+    sprintf(node_names[team_id], "%s%i", NODE_NAME_PREFIX, team_id);
+    sprintf(cookies[team_id], "%s%i", COOKIE_PREFIX, team_id);
 
-    if (ei_connect_init(&ec, node_name, cookie, team_id) < 0) {
+    if (ei_connect_init(&ecs[team_id], node_names[team_id], cookies[team_id], team_id) < 0) {
         fprintf(stderr, "ERROR when initializing: %d", erl_errno);
         exit(-1);
     }
 
-    sprintf(hq_node, "%s%i@%s", HQ_NODE, team_id, ei_thishostname(&ec));
+    sprintf(hq_nodes[team_id], "%s%i@%s", HQ_NODE, team_id, ei_thishostname(&ecs[team_id]));
 
-    fprintf(stdout, "this node: %s\n", ei_thisnodename(&ec));
+    fprintf(stdout, "this node: %s\n", ei_thisnodename(&ecs[team_id]));
     // fprintf(stdout, "host: %s\n", ei_thishostname(&ec));
-    fprintf(stdout, "  hq node: %s\n", hq_node);
-    fprintf(stdout, "   cookie: %s\n", cookie);
+    fprintf(stdout, "  hq node: %s\n", hq_nodes[team_id]);
+    fprintf(stdout, "   cookie: %s\n", cookies[team_id]);
 
     return 0;
 }
 
 
-int check_uplink() {
-    if (uplink < 0) {
+int check_uplink(int team_id) {
+    int uplink = uplinks[team_id];
+    ei_cnode ec = ecs[team_id];
+    char* hq_node = hq_nodes[team_id];
+    if (uplink <= 0) {
         uplink = ei_connect_tmo(&ec, hq_node, COMM_TMO);
-        if (uplink < 0) {
+        if (uplink <= 0) {
             if (frame % 600 == 0) {
                 fprintf(stderr, "uplink still not available\n");
             }
+            return -1;
         } else {
             fprintf(stdout, "uplink established on channel %i\n", uplink);
         }
+        uplinks[team_id] = uplink;
     }
-    return uplink;
+    return 0;
 }
 
 
-int send_to_hq(ei_x_buff buff) {
+int send_to_hq(int team_id, ei_x_buff buff) {
     int result = 0;
-    if (check_uplink() >= 0) {
-        fprintf(stdout, "\tsend: ... --[%i]--> hq\n", uplink);
+    if (check_uplink(team_id) >= 0) {
+        //fprintf(stdout, "[%s] --[%i]--> hq\n", ei_thishostname(&ec), uplink);
+        int uplink = uplinks[team_id];
+        ei_cnode ec = ecs[team_id];
         if (ei_reg_send_tmo(&ec, uplink, COMMROOM, buff.buff, buff.index, COMM_TMO) < 0) {
             fprintf(stderr, "\tsend failed: %i\n", erl_errno);
             result = -1;
@@ -93,34 +99,9 @@ int send_to_hq(ei_x_buff buff) {
 }
 
 
-int send_raw_event(int topic, const void* data) {
-    fprintf(stdout, "\n\tsend: %i, data:'%s' --[%i]--> hq\n", topic, (char*)data, uplink);
-
-    erl_errno = 0;
-    ei_x_buff sendbuf;
-    if (ei_x_new_with_version(&sendbuf) < 0) {
-        fprintf(stderr, "\tei_x_new_with_version failed: %i\n", erl_errno);
-    }
-    if (ei_x_encode_tuple_header(&sendbuf, 3) < 0) {
-        fprintf(stderr, "\tei_x_encode_tuple_header failed: %i\n", erl_errno);
-    }
-    if (ei_x_encode_atom(&sendbuf, "event") < 0){
-        fprintf(stderr, "\tei_x_encode_atom failed: %i\n", erl_errno);
-    }
-    if (ei_x_encode_long(&sendbuf, topic) < 0){
-        fprintf(stderr, "\tei_x_encode_long failed: %i\n", erl_errno);
-    }
-    if (ei_x_encode_binary(&sendbuf, data, sizeof(data)) < 0) {
-        fprintf(stderr, "\tei_x_encode_string failed: %i\n", erl_errno);
-    }
-    fprintf(stdout, "\t\tpayload size: %i bytes\n", sendbuf.index);
-
-    return send_to_hq(sendbuf);
-}
-
-
-int send_event(int topic, const void* data) {
-    fprintf(stdout, "\n\tsend: %i, data:'%s' --[%i]--> hq\n", topic, (char*)data, uplink);
+int send_event(int team_id, int topic, const void* data) {
+    int uplink = uplinks[team_id];
+    fprintf(stdout, "[%s] topic %i --[%i]--> %s\n", cookies[team_id], topic, uplink, hq_nodes[team_id]);
 
     erl_errno = 0;
     ei_x_buff sendbuf;
@@ -133,52 +114,29 @@ int send_event(int topic, const void* data) {
         ei_x_free(&sendbuf);
         return 1;
     }
-    /*
-    switch (topic) {
-        case EVENT_UNIT_CREATED: {
-                fprintf(stdout, "EVENT_UNIT_CREATED received\n");
-                struct SUnitCreatedEvent* event_data = (struct SUnitCreatedEvent*)data;
-                ei_x_encode_atom(&sendbuf, "unit_created");
-                ei_x_encode_tuple_header(&sendbuf, 2);
-                ei_x_encode_long(&sendbuf, event_data->unit);
-                ei_x_encode_long(&sendbuf, event_data->builder);
-                break;
-            }
-        case EVENT_UNIT_FINISHED: {
-                fprintf(stdout, "EVENT_UNIT_FINISHED received\n");
-                struct SUnitFinishedEvent* event_data = (struct SUnitFinishedEvent*)data;
-                ei_x_encode_atom(&sendbuf, "unit_finished");
-                ei_x_encode_tuple_header(&sendbuf, 1);
-                ei_x_encode_long(&sendbuf, event_data->unit);
-                break;
-            }
-        default:
-            ei_x_encode_long(&sendbuf, topic);
-            ei_x_encode_binary(&sendbuf, data, sizeof(data));
-    }
-    */
 
-    return send_to_hq(sendbuf);
+    return send_to_hq(team_id, sendbuf);
 }
 
 
-int send_pong() {
+int send_pong(int team_id) {
     ei_x_buff buff;
     ei_x_new_with_version(&buff);
     ei_x_encode_atom(&buff, "pong");
-    return send_to_hq(buff);
+    return send_to_hq(team_id, buff);
 }
 
 
-int send_tick() {
+int send_tick(int team_id, int frame) {
     ei_x_buff buff;
     ei_x_new_with_version(&buff);
     ei_x_encode_atom(&buff, "tick");
-    return send_to_hq(buff);
+    ei_x_encode_long(&buff, frame);
+    return send_to_hq(team_id, buff);
 }
 
 
-int send_atom_intlist_to_hq(const char* atom, int* ls, int size) {
+int send_atom_intlist_to_hq(int team_id, const char* atom, int* ls, int size) {
     ei_x_buff buff;
     ei_x_new_with_version(&buff);
     ei_x_encode_tuple_header(&buff, 2);
@@ -189,11 +147,11 @@ int send_atom_intlist_to_hq(const char* atom, int* ls, int size) {
         ei_x_encode_ulong(&buff, ls[i]);
     }
     ei_x_encode_empty_list(&buff);
-    return send_to_hq(buff);
+    return send_to_hq(team_id, buff);
 }
 
 
-int send_atom_long_atom_string_to_hq(const char* atom1, long l, const char* atom2, const char* s) {
+int send_atom_long_atom_string_to_hq(int team_id, const char* atom1, long l, const char* atom2, const char* s) {
     ei_x_buff buff;
     ei_x_new_with_version(&buff);
     ei_x_encode_tuple_header(&buff, 4);
@@ -201,11 +159,11 @@ int send_atom_long_atom_string_to_hq(const char* atom1, long l, const char* atom
     ei_x_encode_ulong(&buff, l);
     ei_x_encode_atom(&buff, atom2);
     ei_x_encode_string(&buff, s);
-    return send_to_hq(buff);
+    return send_to_hq(team_id, buff);
 }
 
 
-int send_atom_int_atom_float3_to_hq(const char* atom1, int i, const char* atom2, const float* pos) {
+int send_atom_int_atom_float3_to_hq(int team_id, const char* atom1, int i, const char* atom2, const float* pos) {
     ei_x_buff buff;
     ei_x_new_with_version(&buff);
     ei_x_encode_tuple_header(&buff, 6);
@@ -215,32 +173,32 @@ int send_atom_int_atom_float3_to_hq(const char* atom1, int i, const char* atom2,
     ei_x_encode_double(&buff, pos[0]);
     ei_x_encode_double(&buff, pos[1]);
     ei_x_encode_double(&buff, pos[2]);
-    return send_to_hq(buff);
+    return send_to_hq(team_id, buff);
 }
 
-int send_all_unit_ids() {
+int send_all_unit_ids(int team_id) {
     int ids[100];
-    int result = callback->getTeamUnits(team_id, ids, 100);
+    int result = callbacks[team_id]->getTeamUnits(team_id, ids, 100);
     fprintf(stdout, "result: %i\n", result);
-    return send_atom_intlist_to_hq("teamunits", ids, result);
+    return send_atom_intlist_to_hq(team_id, "teamunits", ids, result);
 }
 
 
-int send_unit_name(int unit_id) {
-    int unit_def_id = callback->Unit_getDef(team_id, unit_id);
-    const char* name = callback->UnitDef_getHumanName(team_id, unit_def_id);
-    return send_atom_long_atom_string_to_hq("unit", unit_id, "name", name);
+int send_unit_name(int team_id, int unit_id) {
+    int unit_def_id = callbacks[team_id]->Unit_getDef(team_id, unit_id);
+    const char* name = callbacks[team_id]->UnitDef_getHumanName(team_id, unit_def_id);
+    return send_atom_long_atom_string_to_hq(team_id, "unit", unit_id, "name", name);
 }
 
 
-int unit_pos(int unit_id) {
+int unit_pos(int team_id, int unit_id) {
     float pos[3];
-    callback->Unit_getPos(team_id, unit_id, pos);
-    return send_atom_int_atom_float3_to_hq("unit", unit_id, "pos", pos);
+    callbacks[team_id]->Unit_getPos(team_id, unit_id, pos);
+    return send_atom_int_atom_float3_to_hq(team_id, "unit", unit_id, "pos", pos);
 }
 
 
-int move_unit(int unit_id, float* pos) {
+int move_unit(int team_id, int unit_id, float* pos) {
     struct SMoveUnitCommand move_command = {
         unit_id,
         -1,
@@ -248,14 +206,16 @@ int move_unit(int unit_id, float* pos) {
         INT_MAX,
         pos
     };
-    callback->Engine_handleCommand(team_id, team_id, -1, COMMAND_UNIT_MOVE, &move_command);
+    callbacks[team_id]->Engine_handleCommand(team_id, team_id, -1, COMMAND_UNIT_MOVE, &move_command);
 }
 
 
-int receive_command_from_hq() {
-    if (check_uplink() < 0) {
+int check_for_command_from_hq(int team_id) {
+    if (check_uplink(team_id) < 0) {
         return 0;
     }
+
+    int uplink = uplinks[team_id];
 
     erl_errno = 0;
     erlang_msg msg;
@@ -277,7 +237,7 @@ int receive_command_from_hq() {
                 break;
             case EIO:
                 fprintf(stderr, "IO-Error?!\n");
-                uplink = -1;
+                uplinks[team_id] = -1;
                 break;
             case ETIMEDOUT:
                 break;
@@ -291,17 +251,17 @@ int receive_command_from_hq() {
         fprintf(stdout, "\treceive: %s <--[%i]-- /%i, 0:'%s'\n", msg.toname, uplink, arity, command);
     }
     if (strcmp(command, "ping") == 0) {
-        send_pong();
+        send_pong(team_id);
     } else if (strcmp(command, "send_all_unit_ids") == 0) {
-        send_all_unit_ids();
+        send_all_unit_ids(team_id);
     } else if (strcmp(command, "send_unit_name") == 0) {
         long id;
         ei_decode_long(recvbuf.buff, &index, &id);
-        send_unit_name(id);
+        send_unit_name(team_id, id);
     } else if (strcmp(command, "unit_pos") == 0) {
         long id;
         ei_decode_long(recvbuf.buff, &index, &id);
-        unit_pos(id);
+        unit_pos(team_id, id);
     } else if (strcmp(command, "move") == 0) {
         long id;
         ei_decode_long(recvbuf.buff, &index, &id);
@@ -311,29 +271,27 @@ int receive_command_from_hq() {
         ei_decode_double(recvbuf.buff, &index, &z);
         float pos[3] = {(float)x, (float)y, (float)z};
         fprintf(stdout, "move %li to %f/%f\n", id, pos[0], pos[2]);
-        move_unit(id, pos);
+        move_unit(team_id, id, pos);
     }
 
     return 0;
 }
 
 
-EXPORT(int) handleEvent(int skirmishAIId, int topic, const void* data) {
+EXPORT(int) handleEvent(int team_id, int topic, const void* data) {
     if (topic == 3) {
         frame = *((int*)data);  // I 'love' pointers :o)
         if (frame == 1) {
             fprintf(stdout, "\n                LET THE WAR BEGIN!\n\n");
         }
         if (frame % 6000 == 0) {
-            fprintf(stdout, "frame: %i\n", frame);
-            send_tick();
+            send_tick(team_id, frame);
         }
         if (frame % 60 == 0) {
-            return receive_command_from_hq();
+            return check_for_command_from_hq(team_id);
         }
         return 0;
     }
 
-    //send_raw_event(topic, data);
-    return send_event(topic, data);
+    return send_event(team_id, topic, data);
 }
